369a432afccb63b1482b7988e9a0d2980e85a1c7
saki.mizuno <mizunosaki9@gmail.com>
2023-11-25 20:01:16 +0900

    課題

--- Instagram/CommentViewData.swift
+++ Instagram/CommentViewData.swift
@@ -5,4 +5,37 @@
 //  Created by Saki Mizuno on 2023/11/24.
 //
 
-import Foundation
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class CommentViewData: UIViewController {
+    @IBOutlet weak var CommentTextField: UITextField!
+    @IBOutlet weak var CommentSendButton: UIButton!
+
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var postData:PostData?
+    
+    // 送信ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func cmtSendButton(_ sender: Any) {
+        print("DEBUG_PRINT: コメントボタンがタップされました。")
+
+        // コメントを更新する
+        // 更新データを作成する
+        let commentText = CommentTextField.text!
+        let name = Auth.auth().currentUser!.displayName!
+        let comment = "\(name) : \(commentText)"
+        
+        let updateValue = FieldValue.arrayUnion([comment])
+        
+        // コメントに更新データを書き込む
+        let postRef = Firestore.firestore().collection(Const.PostPath).document(postData!.id)
+        postRef.updateData(["comment": updateValue])
+        
+        //閉じる
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+}
--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -5,4 +5,126 @@
 //  Created by Saki Mizuno on 2023/11/04.
 //
 
-import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // 投稿データを格納する配列
+    var postArray: [PostData] = []
+
+    // Firestoreのリスナー
+    var listener: ListenerRegistration?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // カスタムセルを登録する
+        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("DEBUG_PRINT: viewWillAppear")
+        // ログイン済みか確認
+        if Auth.auth().currentUser != nil {
+            // listenerを登録して投稿データの更新を監視する
+            let postsRef = Firestore.firestore().collection(Const.PostPath).order(by: "date", descending: true)
+            listener = postsRef.addSnapshotListener() { (querySnapshot, error) in
+                if let error = error {
+                    print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
+                    return
+                }
+                // 取得したdocumentをもとにPostDataを作成し、postArrayの配列にする。
+                self.postArray = querySnapshot!.documents.map { document in
+                    let postData = PostData(document: document)
+                    print("DEBUG_PRINT: \(postData)")
+                    return postData
+                }
+                // TableViewの表示を更新する
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("DEBUG_PRINT: viewWillDisappear")
+        // listenerを削除して監視を停止する
+        listener?.remove()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得してデータを設定する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+        cell.setPostData(postArray[indexPath.row])
+
+        // セル内のボタンのアクションをソースコードで設定する
+        cell.likeButton.addTarget(self, action:#selector(handleButton(_:forEvent:)), for: .touchUpInside)
+        
+        //コメント用の追加する
+        cell.CommentButton.addTarget(self, action: #selector(cmtButton(_:forEvent:)), for: .touchUpInside)
+
+        return cell
+
+    }
+    // セル内のボタンがタップされた時に呼ばれるメソッド
+    @objc func handleButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("DEBUG_PRINT: likeボタンがタップされました。")
+
+        // タップされたセルのインデックスを求める
+        let touch = event.allTouches?.first
+        let point = touch!.location(in: self.tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+
+        // 配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+
+        // likesを更新する
+        if let myid = Auth.auth().currentUser?.uid {
+            // 更新データを作成する
+            var updateValue: FieldValue
+            if postData.isLiked {
+                // すでにいいねをしている場合は、いいね解除のためmyidを取り除く更新データを作成
+                updateValue = FieldValue.arrayRemove([myid])
+            } else {
+                // 今回新たにいいねを押した場合は、myidを追加する更新データを作成
+                updateValue = FieldValue.arrayUnion([myid])
+            }
+            // likesに更新データを書き込む
+            let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
+            postRef.updateData(["likes": updateValue])
+        }
+    }
+    //コメント送信ボタンが押された時に呼ばれるメソッドを作る
+    //登録用のデータを作ってupdatedataを作る
+    @objc func cmtButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("DEBUG_PRINT: コメントボタンがタップされました。")
+
+        // タップされたセルのインデックスを求める
+        let touch = event.allTouches?.first
+        let point = touch!.location(in: self.tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+
+        // 配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+        
+        let CommentViewData = self.storyboard?.instantiateViewController(withIdentifier: "comment") as! CommentViewData
+        CommentViewData.postData = postData
+        self.present(CommentViewData, animated: true, completion: nil)
+    
+    }
+
+    
+}
